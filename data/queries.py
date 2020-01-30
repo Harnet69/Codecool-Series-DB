f0e47b2dde273a2be56a6358ced8730d94deb8e9
Harnet69 <harnet69@gmail.com>
2020-01-30 13:09:28 +0100

    Added genres page, routing and query

--- data/queries.py
+++ data/queries.py
@@ -1,5 +1,27 @@
 from data import data_manager
+import psycopg2
+from psycopg2.extensions import AsIs
+import json
+from data import data_manager
 
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+# get show more successful actors (now shows the quantity of shows where actors was participate)
+def get_genres_list():
+    sql_str = "SELECT name as genres FROM genres;"
+    try:
+        return data_manager.execute_select(sql_str)
+    except psycopg2.Error as e:
+        print(e)
+
+# get shows by genre. Dump converts data to json format for JavaScript
+def get_shows_by_genre(showGenre):
+    sql_str = "SELECT shows.title, shows.runtime, shows.trailer, shows.homepage, shows.year, shows.rating FROM shows INNER JOIN show_genres ON shows.id=show_genres.show_id " \
+              "LEFT JOIN genres ON genres.id=show_genres.genre_id WHERE genres.name = %s"
+    try:
+        # print(data_manager.execute_select("SELECT genres.name, shows.title, shows.year, shows.rating  FROM shows INNER JOIN show_genres ON shows.id=show_genres.show_id LEFT JOIN genres ON genres.id=show_genres.genre_id WHERE genres.name = %s", (showGenre,)));
+        return json.dumps(data_manager.execute_select(sql_str, (showGenre,)), indent=4, sort_keys=True, default=str)
+    except psycopg2.Error as e:
+        print(e)
--- main.py
+++ main.py
@@ -14,6 +14,22 @@ def index():
 def design():
     return render_template('design.html')
 
+# shows genres
+@app.route('/genres')
+def genres():
+    genreslist = queries.get_genres_list()
+    return render_template('genres.html', genreslist=genreslist)
+
+
+# routing for getting show by genre
+@app.route('/get-shows-by-genre/<show_genre>')
+def get_show_by_genre(show_genre):
+    shows_by_genre = queries.get_shows_by_genre(show_genre)
+    # print("Flask get a data: ", shows_by_genre)
+    return shows_by_genre
+
+
+
 
 def main():
     app.run(debug=True)
